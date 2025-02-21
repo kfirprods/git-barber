fd912f961307a845bbb6d14015e78e08a75063a5
Kfir <kfir@alignedup.com>
2025-02-20 20:17:05 -0500

    deps

--- formula/git-barber.rb
+++ formula/git-barber.rb
@@ -8,6 +8,11 @@ class GitBarber < Formula
 
     depends_on "python"
 
+    resource "gitdb" do
+        url "https://files.pythonhosted.org/packages/72/94/63b0fc47eb32792c7ba1fe1b694daec9a63620db1e313033d18140c2320a/gitdb-4.0.12.tar.gz"
+        sha256 "5ef71f855d191a3326fcfbc0d5da835f26b13fbcba60c32c21091c349ffdb571"
+    end
+
     resource "click" do
         url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
         sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
