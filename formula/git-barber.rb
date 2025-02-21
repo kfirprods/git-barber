5c3547edcb521a7e066987d2603e1cac65cc9102
Kfir <kfir@alignedup.com>
2025-02-20 19:29:40 -0500

    update formula with deps

--- formula/git-barber.rb
+++ formula/git-barber.rb
@@ -8,6 +8,21 @@ class GitBarber < Formula
 
     depends_on "python"
 
+    resource "click" do
+        url "https://files.pythonhosted.org/packages/source/c/click/click-8.1.8.tar.gz"
+        sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
+    end
+    
+    resource "GitPython" do
+        url "https://files.pythonhosted.org/packages/source/g/gitpython/GitPython-3.1.44.tar.gz"
+        sha256 "c87e30b26253bf5418b01b0660f818967f3c503193838337fe5e573331249269"
+    end
+    
+    resource "inquirer" do
+        url "https://files.pythonhosted.org/packages/source/i/inquirer/inquirer-3.4.0.tar.gz"
+        sha256 "8edc99c076386ee2d2204e5e3653c2488244e82cb197b2d498b3c1b5ffb25d0b"
+    end
+
     def install
       virtualenv_install_with_resources
     end
